98f806eb4b223a01f5db2e0bfff00b406369e848
Dashirov7 <ashirovdaniiar@gmail.com>
2023-04-07 05:14:27 +0600

    parser

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 
+import json
 
-
-
-print("Hello World")
-
-x=5
-
-print(x)
+def search_user(username):
+    result = {}
+    result[username] = []
+    sources = {
+        "github": get_github,
+        "habr": get_habr,
+        "tiktok": get_tiktok,
+        "pikabu": get_pikabu,
+        "reddit": get_reddit,
+        "instagram": get_instagram
+    }
+    for source, func in sources.items():
+        link = func(username)
+        if link:
+            result[username].append(link)
+    with open("results.json", "w") as f:
+        json.dump(result, f, indent=4)
